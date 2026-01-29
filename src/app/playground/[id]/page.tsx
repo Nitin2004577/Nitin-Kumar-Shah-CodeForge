4b0b043e54dd68d18074233d35505f1b820a855c
Nitin Kumar Shah <shahnitin500@gmail.com>
2026-01-29 11:31:46 +0545

    feat: Enhance playground page with sidebar and tooltip components

--- src/app/playground/[id]/page.tsx
+++ src/app/playground/[id]/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 import React from "react"
 import { useParams } from "next/navigation";
+import { IdCard, Sidebar } from "lucide-react";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "../../../../src/components/ui/separator";
 
 const Page = () => {
     const {id} = useParams<{id:string}>();
     return (
-        <div>Page</div>
+        <TooltipProvider>
+            <>
+            {/* TODO: TEMPLATEFILE TREE */}
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1"/>
+                        <Separator orientation="vertical" className="mr-2 h-4"/>
+                </header>
+            </SidebarInset>
+            </>
+        </TooltipProvider>
     )
 }
 export default Page;
