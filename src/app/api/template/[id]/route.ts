302497e938bac9d8bb12909a07ea3e2958726d8c
Nitin Kumar Shah <shahnitin500@gmail.com>
2026-02-02 18:59:54 +0545

    feat: enhance GET route to handle template retrieval and error responses

--- src/app/api/template/[id]/route.ts
+++ src/app/api/template/[id]/route.ts
@@ -3,6 +3,7 @@ import {
   saveTemplateStructureToJson,
 } from "@/../features/playground/lib/path-to-json";
 import { db } from "@../../../src/lib/db";
+import { templatePaths } from "@./../../src/lib/template";
 
 import path from "path";
 import fs from "fs/promises";
@@ -14,11 +15,28 @@ export async function GET(
 ) {
   const { id } = await params;
   if (!id) {
-    return Response.json({ error: "Missing Template ID" }, { status: 400 });
+    return Response.json({ error: "Missing Playground ID" }, { status: 400 });
   }
   const playground = await db.playground.findUnique({
     where: {
       id,
     },
   });
+
+  if (!playground) {
+    return Response.json({ error: "Playground not found" }, { status: 404 });
+  }
+  const templateKey = playground.template as keyof typeof templatePaths;
+  const templatePath = templatePaths[templateKey];
+
+  if (!templatePath) {
+    return Response.json({ error: "Invalid template path" }, { status: 400 });
+  }
+
+  try {
+    const inputPath = path.join(process.cwd(), templatePath);
+    const outputFiles = path.join(process.cwd(), `output/${templateKey}.json`);
+
+    await saveTemplateStructureToJson(inputPath, outputFiles)
+  } catch (error) {}
 }
