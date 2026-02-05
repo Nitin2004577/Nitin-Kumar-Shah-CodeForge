674bccd4c81cb491b44b03c1435b7d625824c68e
Nitin Kumar Shah <shahnitin500@gmail.com>
2026-02-05 10:58:42 +0545

    feat: expand TemplateFileTree component with additional imports and type definitions

--- features/playground/components/template-file-tree.tsx
+++ features/playground/components/template-file-tree.tsx
@@ -1,11 +1,109 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
+import * as React from "react";
+import {
+  ChevronRight,
+  File,
+  Folder,
+  Plus,
+  FilePlus,
+  FolderPlus,
+  MoreHorizontal,
+  Trash2,
+  Edit3,
+} from "lucide-react";
 
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarMenuSub,
+  SidebarRail,
+} from "@/components/ui/sidebar";
 
-const TemplateFileTree = () => {
-  return (
-    <div>TemplateFileTree</div>
-  )
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { string } from "zod";
+
+// Using the provided interfaces
+interface TemplateFile {
+  filename: string;
+  fileExtension: string;
+  content: string;
+}
+/**
+ * Represents a folder in the template structure which can caontain files and subfolders
+ */
+interface TemplateFolder {
+  folderName: string;
+  items: (TemplateFile | TemplateFolder)[];
 }
 
-export default TemplateFileTree
+// Union type for items in the file system
+type TemplateItem = TemplateFile | TemplateFolder;
+
+interface TemplateFileTreeProps {
+  data: TemplateItem;
+  onFileSelect?: TemplateFile;
+  selectedFile?: TemplateFile;
+  title?: string;
+  onAddFile?: (file: TemplateFile, parentPath: string[]) => void;
+  onAddFolder?: (folder: TemplateFolder, parentPath: string[]) => void;
+  onDeleteItem?: (file: TemplateItem, parentPath: string[]) => void;
+  onDeleteFolder?: (folder: TemplateFolder, parentPath: string[]) => void;
+  onRenameItem?: (
+    file: TemplateItem,
+    newName: string,
+    parentPath: string[]
+  ) => void;
+  onRenameFolder?: (
+    folder: TemplateFolder,
+    newName: string,
+    parentPath: string[]
+  ) => void;
+}
+
+const TemplateFileTree = () => {
+  return <div>TemplateFileTree</div>;
+};
+
+export default TemplateFileTree;
