0e41a35d59ce86c086cdfbedc7d6113644792db6
Nitin Kumar Shah <shahnitin500@gmail.com>
2026-02-06 21:48:07 +0545

    feat: configure github and google oauth providers

--- auth.config.ts
+++ auth.config.ts
@@ -2,25 +2,26 @@
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
-console.log(
-  "DEBUG AUTH:",
-  process.env.GOOGLE_CLIENT_ID ? "ID Found" : "ID MISSING"
-);
+
 export default {
   providers: [
-    // auth.config.ts
     Google({
-      // This checks for BOTH possible names in your .env
-      clientId: process.env.GOOGLE_CLIENT_ID || process.env.GOOGLE_ID,
-      clientSecret:
-        process.env.GOOGLE_CLIENT_SECRET || process.env.GOOGLE_SECRET,
-      allowDangerousEmailAccountLinking: true,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      // PUT IT HERE INSTEAD
+      allowDangerousEmailAccountLinking: true, 
+      authorization: {
+        params: { prompt: "select_account" },
+      },
     }),
-
     Github({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      // AND HERE
+      allowDangerousEmailAccountLinking: true, 
+      authorization: {
+        params: { prompt: "login" },
+      },
     }),
   ],
 } satisfies NextAuthConfig;
