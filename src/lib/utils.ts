32878ad81c073d04478334dc55f789749cb65993
Nitin Kumar Shah <shahnitin500@gmail.com>
2026-02-10 23:55:52 +0545

    chore: update dependencies and utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,35 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+// import { TemplateFolder } from "../types" // Update the import path to match your project structure
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+
+// // Helper to convert your TemplateFolder structure to WebContainer's FileSystemTree
+// import { FileSystemTree } from "@webcontainer/api";
+
+// const convertToWebContainerTree = (data: TemplateFolder): FileSystemTree => {
+//   const tree: FileSystemTree = {};
+
+//   data.items.forEach((item) => {
+//     if ("folderName" in item) {
+//       // It's a folder
+//       tree[item.folderName] = {
+//         directory: convertToWebContainerTree(item),
+//       };
+//     } else {
+//       // It's a file
+//       tree[`${item.filename}.${item.fileExtension}`] = {
+//         file: {
+//           contents: item.content || "",
+//         },
+//       };
+//     }
+//   });
+
+//   return tree;
+// };
